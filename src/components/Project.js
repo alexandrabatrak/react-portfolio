71bbbe2b50f5acc2fccac095b284ac25bec4a260
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-09 00:56:03 +0000

    Add projects components and json

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,1 +1,16 @@
-export default function Project() {}
+import projects from './projects.json';
+import { Card } from 'react-bootstrap';
+
+export default function Project({ id, title, deployment, repo, img }) {
+  return (
+    <div className='d-flex flex-column'>
+      {/* https://stackoverflow.com/questions/39195687/setting-a-backgroundimage-with-react-inline-styles */}
+      <div className='project-image'>
+        <div className='bg-image' style={{ backgroundImage: `url(${img})` }} />
+      </div>
+      <h3>{title}</h3>
+      <a href='{repo}'>Repo</a>
+      <a href='{deployment}'>Deployment</a>
+    </div>
+  );
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,31 @@
-import Router from './router/Router';
+// import Router from './router/Router';
+import projects from './projects.json';
+import Project from './Project';
 import { Container, Row } from 'react-bootstrap';
 
 export default function Projects() {
   return (
-    <Container fluid>
-      <Row>
-        <div className='bg-secondary d-flex align-items-center min-vh-100 px-5'>
+    <section className='bg-secondary d-flex align-items-center min-vh-100 p-5'>
+      <Container style={{ 'max-width': '1200px' }}>
+        <Row>
           <div>
-            <h1 className='display-1'>Projects</h1>
+            <h2 className='display-1'>Projects</h2>
           </div>
-        </div>
-      </Row>
-    </Container>
+          <div className='grid'>
+            {projects.map((project) => {
+              return (
+                <Project
+                  id={project.id}
+                  title={project.title}
+                  deployment={project.deployment}
+                  repo={project.repo}
+                  img={project.img}
+                />
+              );
+            })}
+          </div>
+        </Row>
+      </Container>
+    </section>
   );
 }
