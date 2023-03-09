0d3123a0d0a113f1958c427fd9f017d7cb96108f
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-09 05:48:15 +0000

    Add copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <p>
+        Coded with * Copyright &copy; {new Date().getFullYear()} Alexandra
+        Batrak
+      </p>
+    </div>
+  );
 }
