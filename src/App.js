17e005ed8502593f2e759eff1a08c6b7b1e09987
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-10 21:45:47 +0000

    Restructure

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
 import Nav from './components/Nav';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
-import Projects from './components/pages/Projects';
 import Footer from './components/Footer';
+import RoutesWrapper from './components/Routes';
 import './App.scss';
 import { AnimatePresence } from 'framer-motion';
-import CustomCursor from './components/CustomCursor';
-import NoiseBg from './components/NoiseBg';
-
-const withCustomCursor = (Component, hoverable) => {
-  return (props) => (
-    <>
-      <CustomCursor hoverable={hoverable} />
-      <Component {...props} />
-    </>
-  );
-};
-
-const HoverableHome = withCustomCursor(Home, 'a, button');
-const HoverableAbout = withCustomCursor(About, 'a, button');
-const HoverableContact = withCustomCursor(Contact, 'a, button');
-const HoverableProjects = withCustomCursor(Projects, 'a, button');
+import SoundIcon from './components/SoundIcon';
 
 export default function App() {
-  const location = useLocation();
-
   return (
     <>
-      <div className='bg-black position-fixed z-0'>
-        <NoiseBg width={window.innerWidth} height={window.innerHeight} />
-      </div>
-      <main className='overflow-hidden position-relative z-1'>
-        <Nav />
+      <Nav />
+      <main className='bg-black overflow-hidden position-relative z-1'>
+        <SoundIcon />
         <AnimatePresence mode='wait'>
-          <Routes key={location.pathname} location={location}>
-            <Route exact path='/' element={<HoverableHome />} />
-            <Route path='/about' element={<HoverableAbout />} />
-            <Route path='/contact' element={<HoverableContact />} />
-            <Route path='/projects' element={<HoverableProjects />} />
-          </Routes>
+          <RoutesWrapper />
         </AnimatePresence>
-        <Footer />
       </main>
+      <Footer />
     </>
   );
 }
