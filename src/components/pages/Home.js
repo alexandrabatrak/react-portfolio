9322812f2fe6d0a4155d461fe601b6d53f738a60
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-10 04:10:20 +0000

    Adjust text animation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,10 +6,10 @@ import { motion } from 'framer-motion';
 export default function Home() {
   const [loopN, setLoopN] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toAnimate = ['create', 'develop', 'meep-meep'];
+  const toAnimate = ['create', 'develop', 'meep-meep', 'design'];
   const [text, setText] = useState('');
-  const interval = 500;
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const interval = 300;
+  const [delta, setDelta] = useState(100 - Math.random() * 400);
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -38,7 +38,7 @@ export default function Home() {
     } else if (isDeleting && updatedText === '') {
       setIsDeleting(false);
       setLoopN(loopN + 1);
-      setDelta(100);
+      setDelta(1);
     }
   };
 
@@ -52,13 +52,13 @@ export default function Home() {
         className='d-flex align-items-center min-vh-100 p-5'>
         <Container fluid>
           <Row>
-            <div>
-              <h1 className='display-1 reveal-text'>
-                {`Hey! I `}
+            <div className='masthead'>
+              <h1 className='display-1 text-white fst-italic'>
+                {`I `}
                 <span className='wrap'>{text}</span>
               </h1>
-              <p className='reveal-text'>Lorem ipsum dolore</p>
-              <button className='btn btn-black reveal-text'>
+              <p className='text-white-50'>Lorem ipsum dolore</p>
+              <button className='btn btn-cta btn-outline-light'>
                 Get in touch
               </button>
             </div>
@@ -70,7 +70,7 @@ export default function Home() {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0, y: '200%' }}
         transition={{ ease: 'easeInOut', duration: 1 }}
-        className='bg-primary d-flex align-items-center min-vh-100 p-5'>
+        className='bg-black bg-opacity-75 d-flex align-items-center min-vh-100 p-5'>
         <Container fluid>
           <Projects />
         </Container>
