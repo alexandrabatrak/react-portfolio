617111e10f348604eab177d47da3a3be96820e5a
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-11 22:22:15 +0000

    Update scroll-down

--- src/extra-components/ScrollDown/ScrollDown.js
+++ src/extra-components/ScrollDown/ScrollDown.js
@@ -5,11 +5,13 @@ import './style.scss';
 export default function ScrollDown() {
   return (
     <div className='scroll-down position-absolute bottom-0 d-flex flex-column justify-content-center text-white mb-3'>
-      <p className='text-white text-lowercase fst-italic m-0'>
-        See what I made
-      </p>
       <Link to='projects' spy='true' smooth='true' duration={500} offset={-70}>
-        <MdOutlineSouth />
+        <p className='text-white text-lowercase fst-italic m-0'>
+          See what I made
+        </p>
+        <span>
+          <MdOutlineSouth />
+        </span>
       </Link>
     </div>
   );
