7e9da7902a6c3f28a940a678fa5659a809667500
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-13 16:49:15 +0000

    Add 'hire' section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
 import './style.scss';
 
 export default function Footer() {
   return (
     <footer>
+      <div className='hire-me'>
+        <p>Lemme do it for you</p>
+        <Link to='/contact'>Hire me</Link>
+      </div>
       <p>
         Coded with * Copyright &copy; {new Date().getFullYear()} Alexandra
         Batrak
