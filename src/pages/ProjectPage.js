ff4fb0fe94eb888a72050acabd48fc6caa82ed59
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-13 16:52:14 +0000

    Project style/render/nav

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,5 +1,6 @@
-import { useParams } from 'react-router-dom';
-import MotionSection from '../components/MotionSection/MotionSection';
+import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import Project from '../components/Project/Project';
 import projects from '../projects.json';
 
@@ -7,10 +8,70 @@ export default function ProjectPage() {
   const { projectId } = useParams();
 
   const project = projects.find((project) => project.id === projectId);
+  const currIndex = projects.findIndex((project) => project.id === projectId);
+  const prevIndex = (currIndex - 1 + projects.length) % projects.length;
+  const nextIndex = (currIndex + 1) % projects.length;
+  const prevProject = projects[prevIndex];
+  const nextProject = projects[nextIndex];
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
-    <MotionSection className='projects-page'>
-      <div>{project && <Project id={project.id} />}</div>
-    </MotionSection>
+    <>
+      <motion.section
+        initial={{ opacity: 0, scaleY: 0 }}
+        animate={{ opacity: 1, scaleY: 1 }}
+        exit={{ opacity: 0, scaleY: 0 }}
+        transition={{ ease: 'easeInOut', duration: 0.5 }}
+        className='projects-page'>
+        <div className='back-link-wrapper'>
+          <Link to='/projects' className='back-link'>
+            <i aria-hidden='true'>&larr;</i>
+            <span hidden>Back to Projects</span>
+          </Link>
+        </div>
+        <div>{project && <Project id={project.id} />}</div>
+      </motion.section>
+      <div className='project-navigation'>
+        {prevProject && (
+          <Link
+            to={`/projects/${prevProject.id}`}
+            className='project-navigation-link link-prev'>
+            <div
+              className='project-navigation-image bg-image'
+              style={{
+                backgroundImage: `url(${
+                  prevProject.imgfolder + prevProject.img[0].url
+                })`,
+              }}>
+              <h4>
+                <i aria-hidden='true'>&larr;</i>
+                <span>{prevProject.title}</span>
+              </h4>
+            </div>
+          </Link>
+        )}
+        {nextProject && (
+          <Link
+            to={`/projects/${nextProject.id}`}
+            className='project-navigation-link link-next'>
+            <div
+              className='project-navigation-image bg-image'
+              style={{
+                backgroundImage: `url(${
+                  nextProject.imgfolder + nextProject.img[0].url
+                })`,
+              }}>
+              <h4>
+                <i aria-hidden='true'>&rarr;</i>
+                <span>{nextProject.title}</span>
+              </h4>
+            </div>
+          </Link>
+        )}
+      </div>
+    </>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,26 +2,44 @@ import MotionSection from '../components/MotionSection/MotionSection';
 import Container from '../components/Container/Container';
 import ProjectsGrid from '../components/ProjectsGrid/ProjectsGrid';
 import PageTitle from '../components/PageTitle/PageTitle';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Projects() {
   return (
     <MotionSection className='projects'>
       <PageTitle title={'Projects'} />
       <Container>
-        <p>
-          Explore my portfolio of frontend development projects, where the art
-          of design and technical proficiency converge in perfect harmony. Each
-          project showcases my keen eye for detail and user-centric approach,
-          resulting in intuitive and visually appealing websites and
-          applications.
-        </p>
-        <p>
-          Take a closer look at the inner workings of each project by clicking
-          on them, and witness the design and development process that
-          culminated in their creation.
-        </p>
+        <motion.div
+          initial={{ opacity: 0, y: '50%' }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ ease: 'easeInOut', duration: 0.5, delay: 0.5 }}>
+          <p>
+            Explore my portfolio of frontend development projects, where the art
+            of design and technical proficiency converge in perfect harmony.
+            Each project showcases my keen eye for detail and user-centric
+            approach, resulting in intuitive and visually appealing websites and
+            applications.
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: '50%' }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false }}
+          transition={{ ease: 'easeInOut', duration: 0.5, delay: 0.6 }}>
+          <p>
+            Take a closer look at the inner workings of each project by clicking
+            on them, and witness the design and development process that
+            culminated in their creation.
+          </p>
+          <p className='scroll-down absolute-center'>
+            <span>&darr;</span>
+          </p>
+        </motion.div>
       </Container>
-      <ProjectsGrid />
+      <AnimatePresence mode='wait'>
+        <ProjectsGrid />
+      </AnimatePresence>
     </MotionSection>
   );
 }
