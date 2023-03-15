8f2dfa93f77766c6c5563848a5f6f7164465fd02
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-15 02:22:22 +0000

    Add socials to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,35 @@
 import { Link } from "react-router-dom";
 import Container from "../Container/Container";
+import MotionParagraph from "../Motion/MotionParagraph";
+import SocialIcons from "../SocialIcons/SocialIcons";
+import { RiHeart3Fill } from "react-icons/ri";
 import "./footer.scss";
 
 export default function Footer() {
   return (
     <footer>
       <Container>
-        <div className='hire-me'>
-          <p>Lemme do it for you</p>
-          <Link to='/contact'>Hire me</Link>
-        </div>
-        <div className='socials-wrapper'>
-          <ul>
-            <li>*</li>
-            <li>*</li>
-            <li>*</li>
-            <li>*</li>
-          </ul>
-        </div>
-        <div className='copyright'>
-          <p>
-            Coded with * Copyright &copy; {new Date().getFullYear()} Alexandra
-            Batrak
-          </p>
-        </div>
+        <MotionParagraph>
+          <div className='hire-me'>
+            <p>Lemme do it for you</p>
+            <Link to='/contact'>Hire me</Link>
+          </div>
+        </MotionParagraph>
+        <MotionParagraph delay={0.6}>
+          <SocialIcons />
+        </MotionParagraph>
+        <MotionParagraph delay={0.7}>
+          <div className='copyright'>
+            <p>
+              Coded with{" "}
+              <span>
+                <RiHeart3Fill />{" "}
+              </span>
+              All right reserved. &copy; {new Date().getFullYear()}{" "}
+              <a href='https://github.com/alexandrabatrak'>Alexandra Batrak</a>
+            </p>
+          </div>
+        </MotionParagraph>
       </Container>
     </footer>
   );
--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -1,13 +1,42 @@
 import "./socialicons.scss";
+import socials from "../../socials.json";
+import {
+  RiGithubLine,
+  RiLinkedinLine,
+  RiStackOverflowLine,
+  RiCodepenLine,
+  RiMailOpenLine,
+} from "react-icons/ri";
 
 export default function SocialIcons() {
   return (
     <div className='socials-wrapper'>
       <ul>
-        <li>*</li>
-        <li>*</li>
-        <li>*</li>
-        <li>*</li>
+        <li>
+          <a href={socials.github}>
+            <RiGithubLine />
+          </a>
+        </li>
+        <li>
+          <a href={socials.linkedin}>
+            <RiLinkedinLine />
+          </a>
+        </li>
+        <li>
+          <a href={socials.stackoverflow}>
+            <RiStackOverflowLine />
+          </a>
+        </li>
+        <li>
+          <a href={socials.codepen}>
+            <RiCodepenLine />
+          </a>
+        </li>
+        <li>
+          <a href={socials.email}>
+            <RiMailOpenLine />
+          </a>
+        </li>
       </ul>
     </div>
   );
