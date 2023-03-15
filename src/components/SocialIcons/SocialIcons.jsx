cf73184dae95716bf418cdabbb43d266c597dc78
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-15 19:40:21 +0000

    Update socials data

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -13,27 +13,27 @@ export default function SocialIcons() {
     <div className='socials-wrapper'>
       <ul>
         <li>
-          <a href={socials.github}>
+          <a href={socials.github} target='_blank' rel='noreferrer'>
             <RiGithubLine />
           </a>
         </li>
         <li>
-          <a href={socials.linkedin}>
+          <a href={socials.linkedin} target='_blank' rel='noreferrer'>
             <RiLinkedinLine />
           </a>
         </li>
         <li>
-          <a href={socials.stackoverflow}>
+          <a href={socials.stackoverflow} target='_blank' rel='noreferrer'>
             <RiStackOverflowLine />
           </a>
         </li>
         <li>
-          <a href={socials.codepen}>
+          <a href={socials.codepen} target='_blank' rel='noreferrer'>
             <RiCodepenLine />
           </a>
         </li>
         <li>
-          <a href={socials.email}>
+          <a href={socials.email} target='_blank' rel='noreferrer'>
             <RiMailOpenLine />
           </a>
         </li>
