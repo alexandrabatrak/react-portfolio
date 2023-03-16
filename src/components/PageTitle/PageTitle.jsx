a8da87b138ed3afcced5e974b5ce88467c11f9aa
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-16 19:22:45 +0000

    Add animationPresence

--- src/components/PageTitle/PageTitle.jsx
+++ src/components/PageTitle/PageTitle.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import "./pagetitle.scss";
 
 export default function PageTitle({ title }) {
@@ -6,29 +6,31 @@ export default function PageTitle({ title }) {
     return Math.random() * (max - min) + min;
   }
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ ease: "easeInOut", duration: 1, delay: 0.5 }}
-      className='heading-wrapper'>
-      <h2>
-        {title.split("").map((letter, i) => (
-          <motion.span
-            key={i}
-            initial={{ perspective: 800, rotateX: 0 }}
-            whileHover={{
-              scaleY: rand(1.1, 1.3),
-              scaleX: rand(1.4, 1.7),
-              scaleZ: rand(1.1, 1.4),
-              perspective: 500,
-              rotateX: rand(-10, 10),
-              transition: { ease: "easeInOut", duration: 0.5 },
-            }}>
-            {letter}
-          </motion.span>
-        ))}
-      </h2>
-    </motion.div>
+    <AnimatePresence>
+      <motion.div
+        key={title}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: "easeInOut", duration: 1, delay: 0.5 }}
+        className='heading-wrapper'>
+        <h2>
+          {title.split("").map((letter, i) => (
+            <motion.span
+              key={i}
+              initial={{ rotateX: 0 }}
+              whileHover={{
+                scaleY: rand(1, 1.3),
+                scaleX: rand(1.4, 1.7),
+                scaleZ: rand(1.1, 1.4),
+                rotateX: rand(-10, 10),
+                transition: { ease: "easeInOut", duration: 0.5 },
+              }}>
+              {letter}
+            </motion.span>
+          ))}
+        </h2>
+      </motion.div>
+    </AnimatePresence>
   );
 }
