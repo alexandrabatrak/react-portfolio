d3dfaa06dfb3b3f0d50d10b1c118fff864367173
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-17 00:09:23 +0000

    Trying to fix lack of exit animation, testing testing whatever

--- src/components/ProjectsGrid/ProjectsGrid.jsx
+++ src/components/ProjectsGrid/ProjectsGrid.jsx
@@ -1,41 +1,67 @@
-import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+// import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { Link, useParams, useLocation } from "react-router-dom";
 import projects from "../../projects.json";
 import "./projectgrid.scss";
 
-export default function ProjectsGrid({ props }) {
+export default function ProjectsGrid() {
+  // const { currentProjectId } = useLocation().state || {};
+  // const { projectId } = useParams();
+  // const [scrollPosition, setScrollPosition] = useState(0);
+
+  // useEffect(() => {
+  //   if (currentProjectId && currentProjectId === projectId) {
+  //     const projectIndex = projects.findIndex(
+  //       (project) => project.id === projectId
+  //     );
+  //     const gridElement = document.getElementById("projects-grid");
+  //     const projectElement = gridElement.children[projectIndex];
+  //     const { top } = projectElement.getBoundingClientRect();
+  //     setScrollPosition(gridElement.scrollTop + top);
+  //   }
+  // }, [currentProjectId, projectId]);
+
+  // useEffect(() => {
+  //   if (scrollPosition !== 0) {
+  //     window.scrollTo({ top: scrollPosition, left: 0, behavior: "smooth" });
+  //     setScrollPosition(0);
+  //   }
+  // }, [scrollPosition]);
+
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ ease: "easeInOut", duration: 0.7, delay: 0.2 }}
-      className='grid'
-      id='projects-grid'>
-      {projects.map((project) => {
-        return (
-          <div className='grid-item' key={project.id}>
-            <Link to={`/projects/${project.id}`}>
-              <motion.div
-                layoutId={project.id}
-                initial={{ opacity: 0, y: "50%" }}
-                whileInView={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, scaleX: 0 }}
-                viewport={{ once: false }}
-                transition={{ ease: "easeInOut", duration: 0.5 }}
-                className='project-wrapper'>
-                <div className='project-image'>
-                  <img
-                    src={project.imgfolder + project.img[0].url}
-                    alt={project.img[0].alt}
-                  />
+    <AnimatePresence mode='wait'>
+      <motion.div
+        initial={{ opacity: 0, y: "50%", scaleY: 1 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 1, scaleY: 0 }}
+        className='grid'
+        id='projects-grid'
+        key={"projects-grid"}>
+        {projects.map((project) => {
+          return (
+            <motion.div
+              initial={{ opacity: 0, y: "50%" }}
+              whileInView={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0 }}
+              viewport={{ once: false }}
+              transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
+              className='grid-item'
+              key={project.id}>
+              <Link to={`/projects/${project.id}`}>
+                <div className='project-wrapper'>
+                  <div className='project-image'>
+                    <img
+                      src={project.imgfolder + project.img[0].url}
+                      alt={project.img[0].alt}
+                    />
+                  </div>
+                  <h3>{project.title}</h3>
                 </div>
-                <h3>{project.title}</h3>
-              </motion.div>
-            </Link>
-          </div>
-        );
-      })}
-    </motion.div>
+              </Link>
+            </motion.div>
+          );
+        })}
+      </motion.div>
+    </AnimatePresence>
   );
 }
