cf51560503da42268de836a911991ad7ee7f8bf3
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-17 03:24:01 +0000

    Add parallax on scroll to the frame-img section

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -1,19 +1,24 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import "./homesection.scss";
 
-// TODO: Add parallax on scroll to the frame-img section
-
 export default function HomeSection() {
+  const { scrollYProgress } = useScroll();
+  const y = useTransform(scrollYProgress, [0, 1], [-50, 50], { clamp: false });
+  const y2 = useTransform(scrollYProgress, [0, 1], [-10, 10], { clamp: false });
+  const x = useTransform(scrollYProgress, [0, 1], [-25, 25], { clamp: false });
+  const x2 = useTransform(scrollYProgress, [0, 1], [-50, 50], { clamp: false });
+
   return (
     <motion.div
-      initial={{ scaleY: 0 }}
-      exit={{ opacity: 0, scaleY: 0 }}
-      whileInView={{ opacity: 1, scaleY: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
       viewport={{ once: true }}
       transition={{ ease: "easeInOut", duration: 0.5 }}>
-      <div className='grid'>
-        <div className='home-frame'>
+      <div className='home-grid'>
+        <motion.div style={{ y, x2 }} className='home-frame'>
           <motion.p
+            style={{ y2, x }}
             initial={{ opacity: 0 }}
             exit={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
@@ -22,21 +27,9 @@ export default function HomeSection() {
             A design enthusiast with exceptional attention to detail, plenty of
             patience and a desire to create beautiful solutions.
           </motion.p>
-        </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          exit={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ ease: "easeInOut", duration: 0.5, delay: 0.7 }}
-          className='face'>
-          <motion.div
-            initial={{ opacity: 0 }}
-            exit={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ ease: "easeInOut", duration: 0.5, delay: 1 }}
-            className='image'>
+        </motion.div>
+        <motion.div style={{ y, x }} className='face'>
+          <motion.div className='image'>
             <img src='./face.jpg' alt='' />
           </motion.div>
         </motion.div>
