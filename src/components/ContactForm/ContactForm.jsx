b4477e1d9bd773207310bb94a7abf303c79617fe
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-17 00:07:29 +0000

    Contact form validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import MotionParagraph from "../Motion/MotionParagraph";
 import { motion } from "framer-motion";
+import { RiCheckLine, RiCloseLine } from "react-icons/ri";
 import "./contactform.scss";
 
 export default function ContactForm() {
   const [state, handleSubmit] = useForm("xnqyzqjz");
   const [sent, setSent] = useState(false);
+  const [error, setError] = useState(false);
 
   const form = document.querySelector("form");
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     await handleSubmit(e);
-    setSent(true);
-    form.reset();
-    setTimeout(() => setSent(false), 1000);
+    if (state.succeeded) {
+      setError(false);
+      setSent(true);
+      form.reset();
+      setTimeout(() => setSent(false), 1000);
+    } else {
+      setError(true);
+    }
   };
 
   return (
     <div className='form-wrapper'>
-      <form onSubmit={handleFormSubmit}>
+      <form name='contact' onSubmit={handleFormSubmit}>
+        <input type='text' name='zaschitaotpridurka' className='inputne' />
         <MotionParagraph>
           <div className='input'>
             <label htmlFor='name'>Name</label>
             <input id='name' type='text' name='name' />
+            <ValidationError prefix='Name' field='name' errors={state.errors} />
           </div>
         </MotionParagraph>
         <MotionParagraph>
@@ -42,10 +51,35 @@ export default function ContactForm() {
           <div className='input'>
             <label htmlFor='message'>Message</label>
             <textarea id='message' type='text' name='message' />
+            <ValidationError
+              prefix='Message'
+              field='message'
+              errors={state.errors}
+            />
           </div>
         </MotionParagraph>
+
         <MotionParagraph>
           <div className='submit-wrapper'>
+            {(sent || error) && (
+              <div
+                className={`submit-feedback ${
+                  state.succeeded ? "success" : "error"
+                }`}>
+                <p className='text-font'>
+                  {state.errors && (
+                    <i aria-hidden='true'>
+                      {state.succeeded ? <RiCheckLine /> : <RiCloseLine />}
+                    </i>
+                  )}
+                  {state.succeeded
+                    ? `Thank you! I'll get back to you as soon as possible.`
+                    : state.errors
+                    ? state.errors[0].message
+                    : ""}
+                </p>
+              </div>
+            )}
             <small>
               By submitting this form, you agree to{" "}
               <a href='/privacy-policy'>Privacy Policy</a>. Your data will be
