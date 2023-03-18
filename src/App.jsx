c8685b9c47a0643a8c1243b2be0b73c96ba6d6e2
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-18 16:08:35 +0000

    Add sound trigger and loading screen base

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
+import { useState } from "react";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
 import RoutesWrapper from "./routes/Routes";
 import MotionFade from "./components/Motion/MotionFade";
+import sound from "./sound/background.mp3";
+import SoundPlayer from "./components/SoundPlayer/SoundPlayer";
 import "./App.scss";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const handleButtonClick = () => {
+    setLoading(false);
+    setIsPlaying(true);
+  };
+
   return (
     <>
-      <Nav />
-      <main>
-        <RoutesWrapper />
-      </main>
-      <MotionFade delay={0.5}>
-        <Footer />
-      </MotionFade>
+      {loading ? (
+        <div className='loader'>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}>
+            <button onClick={handleButtonClick} style={{ zIndex: "1005" }}>
+              Start
+            </button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Nav />
+          <SoundPlayer url={sound} isPlaying={isPlaying} />
+          <main>
+            <RoutesWrapper />
+          </main>
+          <MotionFade delay={0.5}>
+            <Footer />
+          </MotionFade>
+        </>
+      )}
     </>
   );
 }
-
-// GLOBAL TODO:
-// 1 Always define explicit defaultProps for all non-required props.
-// Why? propTypes are a form of documentation, and providing defaultProps means the reader of your code doesn’t have to assume as much. In addition, it can mean that your code can omit certain type checks.
-// 2 Use spread props sparingly.
-// Why? Otherwise you’re more likely to pass unnecessary props down to components. And for React v15.6.1 and older, you could pass invalid HTML attributes to the DOM.
