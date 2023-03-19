7f841553ca9f904b77f128f368caecad8f310b95
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-19 00:48:47 +0000

    Update nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 
